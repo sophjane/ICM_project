94d3b65f11761657f3c58fef057ff3a770bae61b
sophjane <sophiepousinho@gmail.com>
2022-06-01 16:01:14 +0100

    added calendar to main activity

--- MedAssistant/app/src/main/java/ua/icm/medassistant/MainActivity.java
+++ MedAssistant/app/src/main/java/ua/icm/medassistant/MainActivity.java
@@ -3,12 +3,28 @@ package ua.icm.medassistant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.getTime());
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        System.out.println("Day: " + day);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        System.out.println("Hour: " + hour);
+        int minute = calendar.get(Calendar.MINUTE);
+        System.out.println("Minute: " + minute);
+
+        //Log.d(LOG_TAG, String.valueOf(calendar.getTime()));
+
     }
 }
