1ef51fe16365873b250d0c88432625119127ec04
Patricia <pmd8@ua.pt>
2022-06-22 16:37:59 +0100

    final

--- MedAssistant/app/src/main/java/ua/icm/medassistant/datamodel/City.java
+++ MedAssistant/app/src/main/java/ua/icm/medassistant/datamodel/City.java
@@ -7,7 +7,7 @@ import com.google.gson.annotations.SerializedName;
 import java.util.Date;
 import java.util.List;
 
-public class Medication {
+public class City {
 
     @Expose
     @SerializedName("local")
